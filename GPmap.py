e7cacd2466e7cd930ca849d491b9a248f527078f
Sweetroll97 <tiger1997@hotmail.se>
2019-04-08 11:29:34 +0200

    Added more logic with trajectories and further improvment of csv reading

--- GPmap.py
+++ GPmap.py
@@ -1,40 +1,71 @@
-import numpy as np;
-import statistics as ss;
-import pandas as ps;
-import csv;
+import numpy as np
+import statistics as ss
+import pandas as ps
+import csv
+import matplotlib.pyplot as plt
 
 class trajectory:
-    xs = np.array([], (float));
-    ys = np.array([], (float));
-    timestamp = np.array([], (float));
+    xs = np.array([], (float))
+    ys = np.array([], (float))
+    timestamp = np.array([], (float))
     
     #def __init__(self, id):
     #    self.id = id;
         
-    def add_point(self,x,y, time):
-        self.xs.append(x);
-        self.ys.append(y);
-        self.timestamp.append(time);
+    def add_point(self,time,x,y):
+        self.xs.append(x)
+        self.ys.append(y)
+        self.timestamp.append(time)
         
     def get_trajectory():     
-        print("not implemented");
+        print("not implemented")
         
 class trajectories:
      
     def __init__(self):
-        self.pathdict = {};
+        self.pathdict = {}
         
     def add_trajectory(self,id, trajectory):
-        self.pathdict[id] = trajectory;
+        self.pathdict[id] = trajectory
+    def plot():
+        for id in self.pathdict:
+            plt.scatter(self.pathdict[id].x, self.pathdict[id].y)
+        plt.plot();
+        print("hej");
+
+trajs = trajectories()
+ 
         
 def readcsvfile():
+    global trajs;
+    
     with open('testfile.csv') as data:
         data = csv.reader(data, delimiter=',')
-        linenr = 0;
+        linenr = 0
+        
+        isnewtrajectory = true
+        id
+        #newtrajectory = trajectory();
         for row in data:
-            newtrajectory = trajectory();
-            newtrajectory.add_point(row[0]
+            if(row[0] is "###"):
+                trajs.add_trajectory(id,newtrajectory)
+                newtrajectory = trajectory()
+                isnewtrajectory = true
+                
+            else:
+                if not isnewtrajectory:
+                    newtrajectory.add_point(row[0], row[2],row[3]) 
+                    
+                else:
+                    id = row[1]
+                    newtrajectory = trajectory()
+                    isnewtrajectory = false                    
+                    
+                
             
+            
+                
         #trajectories = {id, trajectory}
 
-#readcsvfile();
+readcsvfile()
+trajs.plot()
