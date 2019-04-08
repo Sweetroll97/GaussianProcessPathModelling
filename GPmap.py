4c33931a8d5910d98cd9d3beb6adf29b288d7a06
Sweetroll97 <tiger1997@hotmail.se>
2019-04-08 10:12:02 +0200

    Small update for reading csv file

--- GPmap.py
+++ GPmap.py
@@ -8,8 +8,8 @@ class trajectory:
     ys = np.array([], (float));
     timestamp = np.array([], (float));
     
-    def __init__(self, id):
-        self.id = id;
+    #def __init__(self, id):
+    #    self.id = id;
         
     def add_point(self,x,y, time):
         self.xs.append(x);
@@ -19,16 +19,22 @@ class trajectory:
     def get_trajectory():     
         print("not implemented");
         
+class trajectories:
+     
+    def __init__(self):
+        self.pathdict = {};
+        
+    def add_trajectory(self,id, trajectory):
+        self.pathdict[id] = trajectory;
+        
 def readcsvfile():
     with open('testfile.csv') as data:
         data = csv.reader(data, delimiter=',')
         linenr = 0;
         for row in data:
-            if(linenr == 0):
-                print(f'column names are {", ".join(row)}');
-                linenr = linenr + 1;
-            else:
-                print(f'\t{row[0]} works in the {row[1]}');
-                linenr = linenr + 1;
-                
-readcsvfile();
+            newtrajectory = trajectory();
+            newtrajectory.add_point(row[0]
+            
+        #trajectories = {id, trajectory}
+
+#readcsvfile();
