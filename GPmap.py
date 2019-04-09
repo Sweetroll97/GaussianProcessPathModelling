6fcfd4e21181c813c143c52c44900356d2ae19f4
JohanFei <johanfeinestam@gmail.com>
2019-04-09 14:07:34 +0200

    Inpoling function added

--- GPmap.py
+++ GPmap.py
@@ -6,17 +6,20 @@ import matplotlib.pyplot as plt
  
 
 class trajectory:
-    
-    
     def __init__(self):
         self.xs = np.array([], (float))
         self.ys = np.array([], (float))
-        self.timestamp = np.array([], (float))
+        self.timestamp = np.array([], (float))       
         
     def add_point(self,time,x,y):
         self.xs = np.append(self.xs,x)
         self.ys = np.append(self.ys,y)
         self.timestamp = np.append(self.timestamp,time)
+        
+    def add_interpol_point(self,time,x,y):
+        self.xs = np.append(self.xs,x)
+        self.ys = np.append(self.ys,y)
+        self.timestamp = np.append(self.timestamp,time)        
         
     def get_trajectory():     
         print("not implemented")
@@ -28,6 +31,41 @@ class trajectories:
         
     def add_trajectory(self,id, trajectory):
         self.pathdict[id] = trajectory
+        
+    def interpol_points(self):
+        max_length = 10000
+        for id in self.pathdict:
+            if len(self.pathdict[id].xs) > max_length:
+                max_length = len(self.pathdict[id].xs)
+        for id in self.pathdict:
+            if len(self.pathdict[id].xs) > 0:
+                #Set first values
+                curr_value = self.pathdict[id].xs[0]
+                y_value = self.pathdict[id].ys[0]
+                
+                #Get max and min values for x
+                local_max = max(self.pathdict[id].xs)
+                local_min = min(self.pathdict[id].xs)
+                
+                #Calculate movement
+                movement = (float)(local_max-local_min)/(max_length-1)
+                
+                if(movement == 0):
+                    continue
+                interp_xs = np.array([], (float))
+                interp_ys = np.array([], (float))   
+                #print("Max: "+str(local_max)+ "Curr_value:" + str(curr_value))
+                while curr_value <= local_max and curr_value >= local_min:
+                    interp_ys=np.append(interp_ys,y_value)
+                    interp_xs=np.append(interp_xs,curr_value)
+                    #Update next step
+                    curr_value = curr_value + movement
+                    y_value = np.interp(curr_value,self.pathdict[id].xs,self.pathdict[id].ys)
+                #Set new interpolated trajectory 
+                self.pathdict[id].xs = interp_xs
+                self.pathdict[id].ys = interp_ys
+                
+        
     def plot(self):
         
         plt.axis([-50000,50000.0,-50000.0,50000.0]) # xmin, xmax, ymin, ymax
@@ -65,6 +103,7 @@ def readcsvfile(numoftrajstoread=0):
                     newtrajectory.add_point(row[0], int(row[2]),int(row[3])) 
                     
                 else:
+                    
                     id = row[1]
                     newtrajectory = trajectory()
                     isnewtrajectory = False                    
@@ -77,5 +116,6 @@ def readcsvfile(numoftrajstoread=0):
         #trajectories = {id, trajectory}
 
 readcsvfile(500)
+trajs.interpol_points()
 #print(len(trajs.pathdict))
 trajs.plot()
