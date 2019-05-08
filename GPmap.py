63966211fe91b2ddc7a32a6b9d5cd287cb4f2b64
JohanFei <johanfeinestam@gmail.com>
2019-05-08 14:30:22 +0200

    GMM almost done

--- GPmap.py
+++ GPmap.py
@@ -15,44 +15,14 @@ from sklearn import gaussian_process
 import sklearn.mixture as mixture
 #from sklearn.gaussian_process import 
 from matplotlib.colors import LogNorm
-color_iter = itertools.cycle(['navy', 'c', 'cornflowerblue', 'gold',
-                              'darkorange'])
-
-  
-def plot_results(X, Y_, means, covariances, index, title):
-    splot = plt.subplot(2, 1, 1 + index)
-    col = color_iter
-    for i, (mean, covar, color) in enumerate(zip(
-            means, covariances, color_iter)):
-        v, w = np.linalg.eigh(covar)
-        v = 2. * np.sqrt(2.) * np.sqrt(v)
-        u = w[0] / np.linalg.norm(w[0])
-        # as the DP will not use every component it has access to
-        # unless it needs it, we shouldn't plot the redundant
-        # components.
-        if not np.any(Y_ == i):
-            continue
-        plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
-
-        # Plot an ellipse to show the Gaussian component
-        angle = np.arctan(u[1] / u[0])
-        angle = 180. * angle / np.pi  # convert to degrees
-        ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
-        ell.set_clip_box(splot.bbox)
-        ell.set_alpha(0.5)
-        splot.add_artist(ell)
-
-    plt.xticks(())
-    plt.yticks(())
-    plt.title(title)
 
 class trajectory:
     def __init__(self):
         self.xs = np.array([], (float))
         self.ys = np.array([], (float))
         self.points = np.empty([0,3])#np.array([], (float), ndmin=2)
-        
         self.timestamp = np.array([], (float))
+        self.em_belongs_to_cluster = ""
         
     def add_point(self,time,x,y):
         self.xs = np.append(self.xs,x)
@@ -289,75 +259,130 @@ class trajectories:
 class GaussianMixtureModel():
         
 
-    def __init__(self, trajs,K,sigma):
+    def __init__(self, trajs,K,sigma,M):
         self.trajs = trajectories()
         self.trajs = copy.deepcopy(trajs)
         self.T = len(self.trajs.pathdict[next(iter(self.trajs.pathdict.items()))[0]].points)
+        self.M = M
+        self.beta = self.T/K
         self.K = K
-        self.beta = self.T/self.K
         self.sigma = sigma
         self.EM_clusters = {}
         self.set_init_em_centroid_points()
         
     def set_init_em_centroid_points(self):
-        curr_list = rdm.sample(list(self.trajs.pathdict.keys()),self.K)
+        curr_list = rdm.sample(list(self.trajs.pathdict.keys()),self.M)
         cluster_name = "cluster "
         for i, obj in enumerate(curr_list):
             number = str((i+1))
             self.EM_clusters[cluster_name+number] = [obj]
-        trajs = rdm.sample(list(self.trajs.pathdict.keys()),self.K)
-        for traj in trajs:
-            self.EM_clusters[cluster_name+str(1)].append(traj)
-        self.plot_cluster()
-        self.get_mean_cluster_i("cluster 1")    
+            self.trajs.pathdict[obj].em_belongs_to_cluster = cluster_name+number
+  
         
         
         
     def plot_cluster(self,key= "##"):
         if key == "##": #Print all trajectories
-            for id in self.EM_clusters:
-                for key in self.EM_clusters[id]:
-                    a = self.trajs.pathdict[key].points
-                    plt.plot(a[:][0],a[:][1])
+            for traj_keys in self.EM_clusters:
+                #mean = np.empty([1, self.T, 3])
+                #test = self.get_mean_cluster_iT(traj_keys)
+                #mean = np.append(mean,self.get_mean_cluster_iT(traj_keys), axis=0)
+                mean = np.array(self.get_mean_cluster_iT(traj_keys))
+                plt.plot(mean[:,0],mean[:,1])
         else: #Print specific trajectory
-            for trajecory in self.trajectories.pathdict[key]:
-                plt.plot(trajecory.points[:0][-1],trajecory.points[:1][-1]) 
-        plt.show()
+            for traj in self.trajs.pathdict[key]:
+                plt.plot(traj.points[:,0],trajecory.points[:,1]) 
         
-       
+    
+    def get_mean_cluster_iT(self,key):
+        keys = self.EM_clusters[key]
+        cluster_size = len(keys)
+        if cluster_size == 0:
+            return [1,1,0]
+        mean_trajectory = []
         
-    def update_em(self, index, traj):
-        return 0
-        
+        for i in range(0,self.T):
+            sum_from_points = [0,0,0]
+            for t_key in keys:
+                sum_from_points[0] = (sum_from_points[0] + self.trajs.pathdict[t_key].points[i][0])
+                sum_from_points[1] = (sum_from_points[1] + self.trajs.pathdict[t_key].points[i][1])
+                sum_from_points[2] = (sum_from_points[2] + self.trajs.pathdict[t_key].points[i][2])
+            sum_from_points[0] = sum_from_points[0]/len(keys)
+            sum_from_points[1] = sum_from_points[1]/len(keys)
+            sum_from_points[2] = sum_from_points[2]/len(keys)           
+            mean_trajectory.append(sum_from_points)
+        return mean_trajectory    
+    
         
     def get_mean_cluster_i(self,key):
         keys = self.EM_clusters[key]
+        cluster_size = len(keys)        
         mean_trajectory = []
         sum_from_points = [0,0,0]
         for i in range(0,self.T):
-            for key in keys:
-                sum_from_points+= self.trajs.pathdict[key].points[i]
-                a = i%(self.beta)
-            if i%(self.beta) == (self.beta - 1):
-                mean_trajectory.append(sum_from_points/(len(keys)*self.K))
+            for t_key in keys:
+                
+                sum_from_points[0] = sum_from_points[0] + self.trajs.pathdict[t_key].points[i][0]
+                sum_from_points[1] = sum_from_points[1] + self.trajs.pathdict[t_key].points[i][1]
+                sum_from_points[2] = sum_from_points[2] + self.trajs.pathdict[t_key].points[i][2]
+            if i%(self.beta) == (self.beta - 1) and cluster_size > 0:
+                sum_from_points[0] = sum_from_points[0]/(cluster_size*self.beta)
+                sum_from_points[1] = sum_from_points[1]/(cluster_size*self.beta)
+                sum_from_points[2] = sum_from_points[2]/(cluster_size*self.beta)
+                mean_trajectory.append(sum_from_points)
                 sum_from_points = [0,0,0]
         
         return mean_trajectory
-                
+    
+    def normalize(self,t1,t2):
+        return math.sqrt((t1[0]-t2[0])**2+(t1[1]-t2[1])**2)
             
+    
+    def nearest_cluster(self,t_key):
+        cluster_key = "cluster 1"
+        highest_prob = 0.0 
+        total_probability = 0.0
+        for key,value in self.EM_clusters.items():
+            #The E-step calculate the Expected value
+            mean = self.get_mean_cluster_i(key)
+            probability = 1.0
+            j = 0            
+            for t in range(self.T):           
+                probability *= math.exp((((-1)/(2*self.sigma**2)))*(self.normalize(self.trajs.pathdict[t_key].points[t][:-1],mean[j][:-1])**2))
+                total_probability+= probability
+                if t%(self.K) == (self.K - 1):                        
+                    j+=1
+            if probability> highest_prob:
+                highest_prob = probability
+                cluster_key = key
+        return cluster_key
+    
+
+    def make_swap(self, old_cluster, new_cluster, key, traj,swap):
+        if(old_cluster != new_cluster):
+            length = 1
+            if old_cluster !="":
+                length = len(self.EM_clusters[old_cluster])
+                if length > 1:
+                    self.EM_clusters[old_cluster].remove(key)
+            if length > 0:
+                self.EM_clusters[new_cluster].append(key)
+                traj.em_belongs_to_cluster = new_cluster
+                swap = True
+        return swap
         
     def GMM(self):
-        #initialize start state
-        #while less than treashold:
-            #calculate mean
-            #update mean to each cluster
-        mean1 = self.get_mean_cluster_i("cluster 1")
-        print(mean1)
-        mean2 = self.get_mean_cluster_i("cluster 2")
-        print(mean2)
-        mean3 = self.get_mean_cluster_i("cluster 3")
-        print(mean3)        
-        
+        swap = True
+        while swap == True:
+            swap = False
+            for key in self.trajs.pathdict:
+                traj = self.trajs.pathdict[key]
+                old_cluster = traj.em_belongs_to_cluster
+                new_cluster = self.nearest_cluster(key)
+                swap = self.make_swap(old_cluster, new_cluster, key,traj,swap)
+            self.plot_cluster()
+            plt.show()
+
             
             
 
@@ -372,7 +397,7 @@ trajs = trajectories()
 def readcsvfile(numoftrajstoread=0):
     global trajs;
     
-    with open('testfile1.csv') as data:
+    with open('testfile.csv') as data:
         data = csv.reader(data, delimiter=',')
         trajnr = 0
         
@@ -380,7 +405,7 @@ def readcsvfile(numoftrajstoread=0):
         id = 0
         for row in data:
             if(row[0] == '###'):
-                if(newtrajectory.get_length() > 0):
+                if(newtrajectory.get_length() > 1000):
                     trajs.add_trajectory(id,newtrajectory)
                     trajnr = trajnr + 1;
                 if numoftrajstoread is not 0 and trajnr >= numoftrajstoread:
@@ -401,31 +426,14 @@ def readcsvfile(numoftrajstoread=0):
 
 
 
-readcsvfile(19)
+readcsvfile(300)
 #trajs.filter_noise()
 trajs.interpol_points(10)
-#traj1 = trajectory()
-#traj1.add_point(0,2.1,8.0)
-#traj2 = trajectory()
-#traj2.add_point(0,2.5,10)
-#traj3 = trajectory()
-#traj3.add_point(0,3.6,12)
-#traj4 = trajectory()
-#traj4.add_point(0,4,14)
-#
-#clus = {}
-#clus["cluster_1"] = traj1
-#clus["cluster_2"] = traj2
-#clus["cluster_3"] = traj3
-#clus["cluster_4"] = traj4
-    
 #trajs.interpol_test(10)
-
 #trajs.plot()
-K = 5
 #test = trajs.kmeansclustering(25)
-sigma = 1700
-GMM = GaussianMixtureModel(trajs,K,sigma)
+K = 5
+M = 20
+sigma = 2000
+GMM = GaussianMixtureModel(trajs,K,sigma,M)
 GMM.GMM()
-#trajs.pathdict[next(iter(trajs.pathdict))]. #test row for a n trajectory
-#trajs.calc_mean_traj([])
